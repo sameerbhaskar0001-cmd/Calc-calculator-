7bd2f16fc9d82a6935565e9da5f76fe8684d77cf
sameerbhaskar0001-cmd <sameerbhaskar0001@gmail.com>
2026-07-27 08:34:43 +0530

    chore: update application launcher icons

--- fix_vibrate.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open("app/src/main/java/com/example/CalculatorViewModel.kt", "r") as f:
-    text = f.read()
-
-text = text.replace("vibrator.vibrate(effect, attributes)", "if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) { vibrator.vibrate(effect, attributes) } else { vibrator.vibrate(effect) }")
-
-with open("app/src/main/java/com/example/CalculatorViewModel.kt", "w") as f:
-    f.write(text)
--- run_test.sh
+++
@@ -1,1 +0,0 @@
-kotlinc -cp "app/build/tmp/kotlin-classes/debug:app/build/tmp/kapt3/classes/debug" test_retrofit.kt
